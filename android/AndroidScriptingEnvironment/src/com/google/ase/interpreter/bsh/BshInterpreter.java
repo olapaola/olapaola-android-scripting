d081b633064b2c7ddf4ebcc7365263829beb1a80
Damon Kohler <damonkohler@gmail.com>
2009-08-10 16:40:31 +0200

    Partially completed support for proc management.

---
+++ android/AndroidScriptingEnvironment/src/com/google/ase/interpreter/bsh/BshInterpreter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.ase.interpreter.bsh;
+
+import com.google.ase.AndroidFacade;
+import com.google.ase.interpreter.AbstractInterpreter;
+import com.google.ase.interpreter.InterpreterProcessInterface;
+
+public class BshInterpreter extends AbstractInterpreter {
+
+  @Override
+  public String getExtension() {
+    return ".bsh";
+  }
+
+  @Override
+  public String getName() {
+    return "bsh";
+  }
+
+  @Override
+  public String getNiceName() {
+    return "BeanShell 2.0b4";
+  }
+
+  @Override
+  public String getContentTemplate() {
+    return "source(\"/sdcard/ase/extras/bsh/android.bsh\");\ndroid = Android();\n";
+  }
+
+  @Override
+  public InterpreterProcessInterface buildProcess(AndroidFacade facade, String scriptName) {
+    return new BshInterpreterProcess(facade, scriptName);
+  }
+
+  @Override
+  public boolean hasInterpreterArchive() {
+    return false;
+  }
+
+  @Override
+  public boolean hasInterpreterExtrasArchive() {
+    return true;
+  }
+
+  @Override
+  public boolean hasScriptsArchive() {
+    return true;
+  }
+
+}
---
+++ android/AndroidScriptingEnvironment/src/com/google/ase/interpreter/jruby/JRubyInterpreter.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.ase.interpreter.jruby;
+
+import com.google.ase.AndroidFacade;
+import com.google.ase.interpreter.AbstractInterpreter;
+import com.google.ase.interpreter.InterpreterProcessInterface;
+
+public class JRubyInterpreter extends AbstractInterpreter {
+
+  @Override
+  public String getExtension() {
+    // TODO(psycho): Add support for multiple interpreters for the same extension later.
+    return ".rb";
+  }
+
+  @Override
+  public String getName() {
+    return "jruby";
+  }
+
+  @Override
+  public String getNiceName() {
+    return "JRuby-1.2.0RC1";
+  }
+
+  @Override
+  public InterpreterProcessInterface buildProcess(AndroidFacade facade, String scriptName) {
+    return new JRubyInterpreterProcess(facade, scriptName);
+  }
+
+  @Override
+  public boolean hasInterpreterArchive() {
+    return false;
+  }
+
+  @Override
+  public boolean hasInterpreterExtrasArchive() {
+    return true;
+  }
+
+  @Override
+  public boolean hasScriptsArchive() {
+    return false;
+  }
+
+}

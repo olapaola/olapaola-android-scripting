4b329557d627bd932d541125fb20294d80daa889
Damon Kohler <damonkohler@gmail.com>
2009-12-23 14:33:37 +0100

    Small cleanups of sendEmail API.

---
+++ android/AndroidScriptingEnvironment/src/com/google/ase/AseAnalytics.java
@@ -0,0 +1,18 @@
+package com.google.ase;
+
+import android.app.Activity;
+
+public class AseAnalytics {
+  private AseAnalytics() {
+    // Utility class.
+  }
+
+  public static void trackActivity(Activity activity, String name) {
+    ((AseApplication) activity.getApplication()).track(name);
+  }
+
+  public static void trackActivity(Activity activity) {
+    String name = activity.getClass().getSimpleName();
+    trackActivity(activity, name);
+  }
+}

2263f951b0ea97411da037b69c4b6eeb47be1513
Damon Kohler <damonkohler@gmail.com>
2010-02-25 16:53:44 +0100

    Fix build.

---
+++ android/AndroidScriptingEnvironment/src/com/google/ase/AndroidProxy.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.ase;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+
+import com.google.ase.facade.AndroidFacade;
+import com.google.ase.facade.MediaFacade;
+import com.google.ase.facade.SpeechRecognitionFacade;
+import com.google.ase.facade.TextToSpeechFacade;
+import com.google.ase.facade.ui.UiFacade;
+import com.google.ase.jsonrpc.JsonRpcServer;
+import com.google.ase.jsonrpc.RpcInfo;
+
+public class AndroidProxy {
+
+  private InetSocketAddress mAddress;
+  private final JsonRpcServer mJsonRpcServer;
+  private final AndroidFacade mAndroidFacade;
+
+  public AndroidProxy(Service service, Intent intent) {
+    mAndroidFacade = new AndroidFacade(service, new Handler(), intent);
+    MediaFacade mediaFacade = new MediaFacade();
+    TextToSpeechFacade ttsFacade = new TextToSpeechFacade(service);
+    SpeechRecognitionFacade srFacade = new SpeechRecognitionFacade(mAndroidFacade);
+    UiFacade uiFacade = new UiFacade(service);
+    mJsonRpcServer = new JsonRpcServer(mAndroidFacade, mediaFacade, ttsFacade, srFacade, uiFacade);
+  }
+
+  public InetSocketAddress getAddress() {
+    return mAddress;
+  }
+
+  public InetSocketAddress startLocal() {
+    mAddress = mJsonRpcServer.startLocal();
+    return mAddress;
+  }
+
+  public InetSocketAddress startPublic() {
+    mAddress = mJsonRpcServer.startPublic();
+    return mAddress;
+  }
+
+  public Map<String, RpcInfo> getKnownRpcs() {
+    return mJsonRpcServer.getKnownRpcs();
+  }
+
+  public void shutdown() {
+    mJsonRpcServer.shutdown();
+  }
+}

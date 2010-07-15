d375146175ae4d926976ddf1fc19fdb39134088d
Damon Kohler <damonkohler@gmail.com>
2010-07-15 11:42:50 +0200

    Some renaming and a small change to the monitor context menu.

---
+++ android/InterpreterForAndroidTemplate/src/com/dummy/fooforandroid/FooDescriptor.java
@@ -0,0 +1,85 @@
+package com.dummy.fooforandroid;
+
+import android.content.Context;
+
+import com.googlecode.android_scripting.interpreter.InterpreterDescriptor;
+
+public class FooDescriptor implements InterpreterDescriptor {
+
+  public String getName() {
+    return null;
+  }
+
+  public String getNiceName() {
+    return null;
+  }
+
+  public String getExtension() {
+    return null;
+  }
+
+  public String getBinary() {
+    return null;
+  }
+
+  public String getPath(Context arg0) {
+    return null;
+  }
+
+  public int getVersion() {
+    return 0;
+  }
+
+  public String getEmptyParams(Context context) {
+    return null;
+  }
+
+  public String getExecuteCommand(Context context) {
+    return null;
+  }
+
+  public String getExecuteParams(Context context) {
+    return null;
+  }
+
+  public boolean hasInterpreterArchive() {
+    return false;
+  }
+
+  public String getInterpreterArchiveName() {
+    return null;
+  }
+
+  public String getInterpreterArchiveUrl() {
+    return null;
+  }
+
+  public boolean hasExtrasArchive() {
+    return false;
+  }
+
+  public String getExtrasArchiveName() {
+    return null;
+  }
+
+  public String getExtrasArchiveUrl() {
+    return null;
+  }
+
+  public boolean hasScriptsArchive() {
+    return false;
+  }
+
+  public String getScriptsArchiveName() {
+    return null;
+  }
+
+  public String getScriptsArchiveUrl() {
+    return null;
+  }
+
+  public String[] getExecuteArgs(Context context) {
+    return null;
+  }
+
+}
---
+++ android/PythonForAndroid/src/com/googlecode/pythonforandroid/PythonMain.java
@@ -0,0 +1,31 @@
+package com.googlecode.pythonforandroid;
+
+import android.content.Context;
+
+
+import com.googlecode.android_scripting.AsyncTaskListener;
+import com.googlecode.android_scripting.InterpreterInstaller;
+import com.googlecode.android_scripting.InterpreterUninstaller;
+import com.googlecode.android_scripting.activity.Main;
+import com.googlecode.android_scripting.exception.Sl4aException;
+import com.googlecode.android_scripting.interpreter.InterpreterDescriptor;
+
+public class PythonMain extends Main {
+
+  @Override
+  protected InterpreterDescriptor getDescriptor() {
+    return new PythonDescriptor();
+  }
+
+  @Override
+  protected InterpreterInstaller getInterpreterInstaller(InterpreterDescriptor descriptor,
+      Context context, AsyncTaskListener<Boolean> listener) throws Sl4aException {
+    return new PythonInstaller(descriptor, context, listener);
+  }
+
+  @Override
+  protected InterpreterUninstaller getInterpreterUninstaller(InterpreterDescriptor descriptor,
+      Context context, AsyncTaskListener<Boolean> listener) throws Sl4aException {
+    return new PythonUninstaller(descriptor, context, listener);
+  }
+}

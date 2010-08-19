fc323cf43c0dc858c31d0f4f4663731654dae585
Alexey Reznichenko <alexey.reznichenko@gmail.com>
2010-08-19 17:39:44 +0200

    Added waitForEvent in EventFacade, onCreateContextMenu and onPrepareOptionsMenu in WebViewTask.

---
+++ android/Utils/src/com/googlecode/android_scripting/FileUtils.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.googlecode.android_scripting;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+
+/**
+ * Utility functions for handling files.
+ * 
+ * @author Damon Kohler (damonkohler@gmail.com)
+ */
+public class FileUtils {
+
+  private FileUtils() {
+    // Utility class.
+  }
+
+  public static int chmod(File path, int mode) throws Exception {
+    Class<?> fileUtils = Class.forName("android.os.FileUtils");
+    Method setPermissions =
+        fileUtils.getMethod("setPermissions", String.class, int.class, int.class, int.class);
+    return (Integer) setPermissions.invoke(null, path.getAbsolutePath(), mode, -1, -1);
+  }
+
+  public static boolean recursiveChmod(File root, int mode) throws Exception {
+    boolean success = chmod(root, mode) == 0;
+    for (File path : root.listFiles()) {
+      if (path.isDirectory()) {
+        success = recursiveChmod(path, mode);
+      }
+      success &= (chmod(path, mode) == 0);
+    }
+    return success;
+  }
+
+  public static void delete(File path) {
+    if (path.isDirectory()) {
+      for (File child : path.listFiles()) {
+        delete(child);
+      }
+      path.delete(); // Delete empty directory.
+    }
+    if (path.isFile()) {
+      path.delete();
+    }
+  }
+
+  public static File copyFromStream(String name, InputStream input) {
+    if (name == null || name.length() == 0) {
+      Log.e("No script name specified.");
+      return null;
+    }
+    if (!makeDirectories(name)) {
+      return null;
+    }
+    File file = new File(name);
+    try {
+      OutputStream output = new FileOutputStream(file);
+      IoUtils.copy(input, output);
+    } catch (Exception e) {
+      Log.e(e);
+      return null;
+    }
+    return file;
+  }
+
+  public static boolean makeDirectories(String filename) {
+    File file = new File(filename);
+    File parent = file.getParentFile();
+    if (!parent.exists()) {
+      Log.v("Creating directory: " + parent.getAbsolutePath());
+      if (!parent.mkdirs()) {
+        Log.e("Failed to create a directory.");
+        return false;
+      }
+    }
+    return true;
+  }
+
+}

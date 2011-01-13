c1201226e469cd5587e578748dab702706a83e58
rjmatthews62 <rjmatthews62@localhost>
2011-01-13 22:13:26 +1100

    Module handling for python (first draft)

---
+++ android/Common/src/com/googlecode/android_scripting/facade/EventFacade.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.googlecode.android_scripting.facade;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import com.googlecode.android_scripting.event.Event;
+import com.googlecode.android_scripting.future.FutureResult;
+import com.googlecode.android_scripting.jsonrpc.RpcReceiver;
+import com.googlecode.android_scripting.rpc.Rpc;
+import com.googlecode.android_scripting.rpc.RpcDefault;
+import com.googlecode.android_scripting.rpc.RpcDeprecated;
+import com.googlecode.android_scripting.rpc.RpcOptional;
+import com.googlecode.android_scripting.rpc.RpcParameter;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This facade exposes the functionality to read from the event queue as an RPC, and the
+ * functionality to write to the event queue as a pure java function.
+ * 
+ * @author Felix Arends (felix.arends@gmail.com)
+ * 
+ */
+public class EventFacade extends RpcReceiver {
+  /**
+   * The maximum length of the event queue. Old events will be discarded when this limit is
+   * exceeded.
+   */
+  private static final int MAX_QUEUE_SIZE = 1024;
+  private final Queue<Event> mEventQueue = new ConcurrentLinkedQueue<Event>();
+  private final CopyOnWriteArrayList<EventObserver> mGlobalEventObservers =
+      new CopyOnWriteArrayList<EventObserver>();
+  private final Multimap<String, EventObserver> mNamedEventObservers =
+      Multimaps.synchronizedListMultimap(ArrayListMultimap.<String, EventObserver> create());
+
+  public EventFacade(FacadeManager manager) {
+    super(manager);
+  }
+
+  @Rpc(description = "Clears all events from the event buffer.")
+  public void eventClearBuffer() {
+    mEventQueue.clear();
+  }
+
+  @Rpc(description = "Returns and removes the oldest n events (i.e. location or sensor update, etc.) from the event buffer.", returns = "A List of Maps of event properties.")
+  public List<Event> eventPoll(
+      @RpcParameter(name = "number_of_events") @RpcDefault("1") Integer number_of_events) {
+    List<Event> events = Lists.newArrayList();
+    for (int i = 0; i < number_of_events; i++) {
+      Event event = mEventQueue.poll();
+      if (event == null) {
+        break;
+      }
+      events.add(event);
+    }
+    return events;
+  }
+
+  @Rpc(description = "Blocks until an event with the supplied name occurs. The returned event is not removed from the buffer.", returns = "Map of event properties.")
+  public Event eventWaitFor(
+      @RpcParameter(name = "eventName") final String eventName,
+      @RpcParameter(name = "timeout", description = "the maximum time to wait (in ms)") @RpcOptional Integer timeout)
+      throws InterruptedException {
+    final FutureResult<Event> futureEvent = new FutureResult<Event>();
+    addNamedEventObserver(eventName, new EventObserver() {
+      @Override
+      public void onEventReceived(Event event) {
+        if (event.getName().equals(eventName)) {
+          synchronized (futureEvent) {
+            if (!futureEvent.isDone()) {
+              futureEvent.set(event);
+              removeEventObserver(this);
+            }
+          }
+        }
+      }
+    });
+    if (timeout != null) {
+      return futureEvent.get(timeout, TimeUnit.MILLISECONDS);
+    } else {
+      return futureEvent.get();
+    }
+  }
+
+  @Rpc(description = "Blocks until an event occurs. The returned event is removed from the buffer.", returns = "Map of event properties.")
+  public Event eventWait(
+      @RpcParameter(name = "timeout", description = "the maximum time to wait") @RpcOptional Integer timeout)
+      throws InterruptedException {
+    final FutureResult<Event> futureEvent = new FutureResult<Event>();
+    synchronized (mEventQueue) { // Anything in queue?
+      if (mEventQueue.size() > 0) {
+        return mEventQueue.poll(); // return it.
+      }
+    }
+    addGlobalEventObserver(new EventObserver() {
+      @Override
+      public void onEventReceived(Event event) { // set up observer for any events.
+        synchronized (futureEvent) {
+          if (!futureEvent.isDone()) {
+            futureEvent.set(event);
+            removeEventObserver(this);
+          }
+          mEventQueue.remove(event);
+        }
+      }
+    });
+    if (timeout != null) {
+      return futureEvent.get(timeout, TimeUnit.MILLISECONDS);
+    } else {
+      return futureEvent.get();
+    }
+  }
+
+  @Rpc(description = "Post an event to the event queue.")
+  public void eventPost(@RpcParameter(name = "name") String name,
+      @RpcParameter(name = "data") String data) {
+    postEvent(name, data);
+  }
+
+  /**
+   * Posts an event with to the event queue.
+   */
+  public void postEvent(String name, Object data) {
+    Event event = new Event(name, data);
+    mEventQueue.add(event);
+    if (mEventQueue.size() > MAX_QUEUE_SIZE) {
+      mEventQueue.remove();
+    }
+    synchronized (mNamedEventObservers) {
+      for (EventObserver observer : mNamedEventObservers.get(name)) {
+        observer.onEventReceived(event);
+      }
+    }
+    synchronized (mGlobalEventObservers) {
+      for (EventObserver observer : mGlobalEventObservers) {
+        observer.onEventReceived(event);
+      }
+    }
+  }
+
+  @RpcDeprecated("eventPost")
+  @Rpc(description = "Post an event to the event queue.")
+  public void postEvent(@RpcParameter(name = "name") String name,
+      @RpcParameter(name = "data") String data) {
+    postEvent(name, data);
+  }
+
+  @RpcDeprecated(value = "eventPoll")
+  @Rpc(description = "Returns and removes the oldest event (i.e. location or sensor update, etc.) from the event buffer.", returns = "Map of event properties.")
+  public Event receiveEvent() {
+    return mEventQueue.poll();
+  }
+
+  @RpcDeprecated("eventWaitFor")
+  @Rpc(description = "Blocks until an event with the supplied name occurs. The returned event is not removed from the buffer.", returns = "Map of event properties.")
+  public Event waitForEvent(
+      @RpcParameter(name = "eventName") final String eventName,
+      @RpcParameter(name = "timeout", description = "the maximum time to wait") @RpcOptional Integer timeout)
+      throws InterruptedException {
+    return eventWaitFor(eventName, timeout);
+  }
+
+  @Override
+  public void shutdown() {
+  }
+
+  public void addNamedEventObserver(String eventName, EventObserver observer) {
+    mNamedEventObservers.put(eventName, observer);
+  }
+
+  public void addGlobalEventObserver(EventObserver observer) {
+    mGlobalEventObservers.add(observer);
+  }
+
+  public void removeEventObserver(EventObserver observer) {
+    mNamedEventObservers.removeAll(observer);
+    mGlobalEventObservers.remove(observer);
+  }
+
+  public interface EventObserver {
+    public void onEventReceived(Event event);
+  }
+}
---
+++ android/ScriptingLayerForAndroid/src/com/googlecode/android_scripting/dialog/Help.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.googlecode.android_scripting.dialog;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
+import com.googlecode.android_scripting.Constants;
+import com.googlecode.android_scripting.R;
+import com.googlecode.android_scripting.interpreter.InterpreterConstants;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Vector;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.connectbot.HelpActivity;
+
+public class Help {
+  private Help() {
+    // Utility class.
+  }
+
+  private static int helpChecked = 0;
+
+  public static boolean checkApiHelp(Context context) {
+    byte[] buf = new byte[1024];
+    if (helpChecked == 0) {
+      try {
+        File dest = new File(InterpreterConstants.SDCARD_SL4A_DOC);
+        if (!dest.exists()) {
+          dest.mkdirs();
+        }
+        new File(InterpreterConstants.SDCARD_SL4A_DOC, "index.html");
+        AssetManager am = context.getAssets();
+        ZipInputStream z = new ZipInputStream(am.open("sl4adoc.zip"));
+        ZipEntry e;
+        while ((e = z.getNextEntry()) != null) {
+          File f = new File(InterpreterConstants.SDCARD_SL4A_DOC, e.getName());
+          if (!f.exists() || f.lastModified() < e.getTime()) {
+            if (!f.exists() && !f.getParentFile().exists()) {
+              f.getParentFile().mkdirs();
+            }
+            OutputStream o = new BufferedOutputStream(new FileOutputStream(f));
+            int len;
+            while ((len = z.read(buf)) > 0) {
+              o.write(buf, 0, len);
+            }
+            o.flush();
+            o.close();
+            f.setLastModified(e.getTime());
+          }
+        }
+        helpChecked = 1;
+      } catch (IOException e) {
+        helpChecked = -1;
+        return false;
+      }
+    }
+    return helpChecked > 0;
+  }
+
+  public static void showApiHelp(Context context, String help) {
+    Intent intent = new Intent();
+    intent.setAction(Intent.ACTION_VIEW);
+    Uri uri = Uri.parse("file://" + InterpreterConstants.SDCARD_SL4A_DOC + help);
+    intent.setDataAndType(uri, "text/html");
+    SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
+    if (p.getBoolean(Constants.FORCE_BROWSER, true)) {
+      intent.setComponent(new ComponentName("com.android.browser",
+          "com.android.browser.BrowserActivity"));
+    }
+    context.startActivity(intent);
+  }
+
+  public static void show(final Activity activity) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+    List<CharSequence> list = new Vector<CharSequence>();
+    list.add("Wiki Documentation");
+    list.add("YouTube Screencasts");
+    list.add("Terminal Help");
+    if (checkApiHelp(activity)) {
+      list.add("API Help");
+    }
+    CharSequence[] mylist = list.toArray(new CharSequence[list.size()]);
+    builder.setItems(mylist, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+        case 0: {
+          Intent intent = new Intent();
+          intent.setAction(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(activity.getString(R.string.wiki_url)));
+          activity.startActivity(intent);
+          break;
+        }
+        case 1: {
+          Intent intent = new Intent();
+          intent.setAction(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(activity.getString(R.string.youtube_url)));
+          activity.startActivity(intent);
+          break;
+        }
+        case 2: {
+          Intent intent = new Intent(activity, HelpActivity.class);
+          activity.startActivity(intent);
+          break;
+        }
+        case 3: {
+          showApiHelp(activity, "index.html");
+        }
+        }
+      }
+    });
+    builder.show();
+  }
+}
